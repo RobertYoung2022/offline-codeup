305e6c1921b142791eea5c048681b86abb79eed1
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-05-05 09:54:19 -0500

    pulled project from folder to push into github

---
+++ Amigoscode-Project/src/main/java/com/example/AmigoscodeProject/AmigoscodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmigoscodeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoscodeProjectApplication.class, args);
+	}
+
+}
---
+++ Amigoscode-Project/src/test/java/com/example/AmigoscodeProject/AmigoscodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmigoscodeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
